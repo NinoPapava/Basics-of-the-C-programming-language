3dd436adf8ba6be16c2332cad1e2f6a7241fdd58
NinoPapava <gv.papava@gmail.com>
2023-09-03 21:13:59 +0400

    Basic Calculator

---
+++ If else Calculator/If else Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_else_Calculator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter first number");
+            double numb1 = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter symbol operation");
+            char symbol = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter second number");
+            double numb2 = short.Parse(Console.ReadLine());
+
+            if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == '%')
+            {
+                if (symbol == '+')
+                {
+                    Console.WriteLine($"{numb1} + {numb2} = {numb1 + numb2}");
+                }
+                else if (symbol == '-')
+                {
+                    Console.WriteLine($"{numb1} - {numb2} = {numb1 - numb2}");
+                }
+                else if (symbol == '*')
+                {
+                    Console.WriteLine($"{numb1} * {numb2} = {numb1 * numb2}");
+                }
+                else if (symbol == '/')
+                {
+                    Console.WriteLine($"{numb1} / {numb2} = {numb1 / numb2}");
+                }
+                else if (symbol == '%')
+                {
+                    Console.WriteLine($"{numb1} % {numb2} = {numb1 % numb2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong Symbol!!");
+            }
+          
+        }
+    }
+}
