4c3abba8e64a5276c232f6d1d4910866834d53af
NinoPapava <gv.papava@gmail.com>
2023-09-03 22:41:07 +0400

    do while do cycles

---
+++ while do while/while do while/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_do_while
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number = 10;
+
+            while (number >= 0)
+            {
+                Console.WriteLine($"number is {number}");
+                number--;
+            }
+
+            string symbol = "y";
+
+            while (symbol == "y") {
+                Console.WriteLine("while");
+                Console.WriteLine("if  you want repeat enter y");
+                symbol = Console.ReadLine();
+            }
+
+            do
+            {
+                Console.WriteLine("do while cycle");
+                Console.WriteLine("if  you want repeat enter y");
+                symbol = Console.ReadLine();
+            } while (symbol == "y");
+        }
+    }
+}
