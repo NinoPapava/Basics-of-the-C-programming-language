72efcec90d48bc80a7d5b181ab4be6de2e79b097
NinoPapava <gv.papava@gmail.com>
2023-09-03 21:53:43 +0400

    switch Calculator

---
+++ switch Calculator/switch Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_Calculator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter First Number");
+            double numb1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Operation [+] [-] [*] [/] [%]");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("EnterSecond Number");
+            double numb2 = double.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"{numb1} + {numb2} = {numb1 + numb2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"{numb1} - {numb2} = {numb1 - numb2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{numb1} * {numb2} = {numb1 * numb2}");
+                    break;
+                case "/":
+                    Console.WriteLine($"{numb1} / {numb2} = {numb1 / numb2}");
+                    break;
+                case "%":
+                    Console.WriteLine($"{numb1} % {numb2} = {numb1 % numb2}");
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong Symbol !!!");
+                    break;
+            }
+        }
+    }
+}
