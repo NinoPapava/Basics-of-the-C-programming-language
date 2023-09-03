4996b093bf345077328578cbcaa55e82b19df41a
NinoPapava <gv.papava@gmail.com>
2023-09-03 19:57:14 +0400

    Print ReadLine

---
+++ Console Read/Console Read/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Read
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, what is your firstname ?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, what is your lastname ?");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine($"{name} {lastname}");
+        }
+    }
+}
