c9ff212f9b0d3f9cfed105f22c325f6e1e5c5d8a
NinoPapava <gv.papava@gmail.com>
2023-09-04 10:47:42 +0400

    create class - Part1

---
+++ Class Part1/Class Part1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Part1
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public string idNumber;
+
+        public void Print()
+        {
+            Console.WriteLine($"name:{name} age:{age} id:{idNumber}");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            person1.name = "nino";
+            person1.age = 22;
+            person1.idNumber = "5945344565";
+
+            person1.Print();
+            
+
+            Person person2 = new Person();
+            person2.name = "gvantsa";
+            person2.age = 26;
+            person2.idNumber = "4845342222";
+
+            person2.Print();
+
+        }
+    }
+}
