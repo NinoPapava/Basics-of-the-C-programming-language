94e4cffa69b8ada30e20257a46be153ebeb807cf
NinoPapava <gv.papava@gmail.com>
2023-09-04 09:17:05 +0400

    for cycle

---
+++ for/for/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace @for
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //int j = 0;
+            //while (j < 10)
+            //{
+            //    Console.WriteLine("hello");
+            //    j++;
+            //}
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("hello");
+            }
+        }
+    }
+}
